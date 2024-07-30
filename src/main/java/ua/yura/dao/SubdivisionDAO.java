ed0965df056bf321c5b84eb7c8deffc17d50c9fc
Yura <wrszxopl>
2024-07-30 21:28:43 +0300

    Commit home 30.07.2024

--- src/main/java/ua/yura/dao/SubdivisionDAO.java
+++ src/main/java/ua/yura/dao/SubdivisionDAO.java
@@ -18,20 +18,20 @@ public class SubdivisionDAO {
         this.pumaList = pumaList;
         this.uclList = uclList;
 
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - ЦУМ", 48, "+38(050) 463-34-01", "Дніпро", "пр.Д Яворницького (пр.Карла Маркса), 52-A"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - 95 квартал", 31, "+38(050) 463-91-16", "Кривий ріг", "пр. Гагаріна, 2"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Глобал Житомир", 62, "+38(050) 386-71-51", "Житомир", "вул. Київська, 77"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Смілянська", 14, "+3(050) 463-91-20", "Черкаси", "вул. Смілянська, 31"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Стрийський", 100, "+38(095) 283-81-37", "Сокільники", "вул. Стрійська, 30-А"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Дружба", 37, "+38(050) 463-44-55", "Львів", "просп. Чорновола, 12"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Перспективна", 130, "+38(050) 923 45 30", "Кропивницький", "вул. Велика Перспективна, 45/9"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Парк", 115, "+38(050) 347-12-15", "Хмельницький", "пр. Миру, 99/2А"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Короленка", 72, "+38(050) 386-09-13", "Рівне", "вул. Борисенка, 1"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Підстанція", 30, "+38(050) 463-91-14", "Дніпро", "пр. Науки, 128"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Мост сіті", 56, "+38(050) 482-98-40", "Дніпро", "вул. Королеви Єлизавети ІІ, 2"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Вокзал Одеса", 19, "+38(050) 463-91-45", "Одеса", "вул.Пантелеймонівська, 17-Б"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Глобал Київ", 63, "+38(050) 386-17-99", "Київ", "вул. Оноре де Бальзака, 2-А"));
-        mcdList.add(new Subdivision(UUID.randomUUID(), "МакДональдз - Опера", 57, "+38(050) 463-16-82", "Львів", "пр. Совбоди, 35"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD ЦУМ", 48, "+38(050) 463-34-01", "Дніпро", "пр.Д Яворницького (пр.Карла Маркса), 52-A"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD 95 квартал", 31, "+38(050) 463-91-16", "Кривий ріг", "пр. Гагаріна, 2"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Глобал Житомир", 62, "+38(050) 386-71-51", "Житомир", "вул. Київська, 77"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Смілянська", 14, "+3(050) 463-91-20", "Черкаси", "вул. Смілянська, 31"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Стрийський", 100, "+38(095) 283-81-37", "Сокільники", "вул. Стрійська, 30-А"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Дружба", 37, "+38(050) 463-44-55", "Львів", "просп. Чорновола, 12"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Перспективна", 130, "+38(050) 923 45 30", "Кропивницький", "вул. Велика Перспективна, 45/9"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Парк", 115, "+38(050) 347-12-15", "Хмельницький", "пр. Миру, 99/2А"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Короленка", 72, "+38(050) 386-09-13", "Рівне", "вул. Борисенка, 1"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Підстанція", 30, "+38(050) 463-91-14", "Дніпро", "пр. Науки, 128"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Мост сіті", 56, "+38(050) 482-98-40", "Дніпро", "вул. Королеви Єлизавети ІІ, 2"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Вокзал Одеса", 19, "+38(050) 463-91-45", "Одеса", "вул.Пантелеймонівська, 17-Б"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Глобал Київ", 63, "+38(050) 386-17-99", "Київ", "вул. Оноре де Бальзака, 2-А"));
+        mcdList.add(new Subdivision(UUID.randomUUID(), "McD Опера", 57, "+38(050) 463-16-82", "Львів", "пр. Совбоди, 35"));
 
         pumaList.add(new Subdivision(UUID.randomUUID(), "Пума - Европа", 61, "+38(056) 786-01-03", "Дніпро", "бул. Європейський, 1-Д, ТЦ “Європа”" ));
         pumaList.add(new Subdivision(UUID.randomUUID(), "Пума - Кінг Кросс", 66, "+38(032) 246 20 67", "с. Сокільники", "вул. Стрийська, 30"));
